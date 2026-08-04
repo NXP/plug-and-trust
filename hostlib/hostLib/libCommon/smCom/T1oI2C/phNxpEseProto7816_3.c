87b78ffa44d0824fc2c28c6d596ad4fbc5d0662b
Jagadish Bogadi Eswaraprasad <jagadish.eswaraprasad@nxp.com>
2026-08-04 17:26:16 +0530

    generic name of variable CRC renamed

--- hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEseProto7816_3.c
+++ hostlib/hostLib/libCommon/smCom/T1oI2C/phNxpEseProto7816_3.c
@@ -115,7 +115,7 @@ static bool_t phNxpEseProto7816_GetRawFrame(void* conn_ctx, uint32_t *data_len, 
 static uint16_t phNxpEseProto7816_ComputeCRC(unsigned char *p_buff, uint32_t offset,
         uint32_t length)
 {
-    uint16_t CAL_CRC = 0xFFFF, CRC = 0x0000;
+    uint16_t CAL_CRC = 0xFFFF, returned_crc = 0x0000;
     uint32_t i = 0;
 
     ENSURE_OR_GO_EXIT(p_buff != NULL);
@@ -137,12 +137,12 @@ static uint16_t phNxpEseProto7816_ComputeCRC(unsigned char *p_buff, uint32_t off
     }
     CAL_CRC ^=0xFFFF;
 #if defined(T1oI2C_UM11225)
-    CRC = ((CAL_CRC & 0xFF) << 8) | ((CAL_CRC >> 8) & 0xFF);
+    returned_crc = ((CAL_CRC & 0xFF) << 8) | ((CAL_CRC >> 8) & 0xFF);
 #elif defined(T1oI2C_GP1_0)
-    CRC = CAL_CRC;
+    returned_crc = CAL_CRC;
 #endif
 exit:
-    return (uint16_t) CRC;
+    return (uint16_t) returned_crc;
 }
 
 /******************************************************************************
